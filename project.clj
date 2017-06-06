f495b0b75ae16e48403ac988efeb957d67c13c15
Michael Lindon <michael.s.lindon@gmail.com>
2017-06-06 17:37:35 -0400

    removed leiningen dep

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "https://github.com/michaellindon/distributions"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-:plugins [[lein-gorilla "0.4.0"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [net.mikera/core.matrix "0.57.0"]
                  [net.mikera/vectorz-clj "0.45.0"]
