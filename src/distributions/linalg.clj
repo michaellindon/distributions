bb9ac9cd4a1c6c9fec10d35e6b394fe24e8d73da
Michael Lindon <michael.s.lindon@gmail.com>
2017-06-18 20:42:09 -0400

    more stable log det

--- src/distributions/linalg.clj
+++ src/distributions/linalg.clj
@@ -1,6 +1,5 @@
 (in-ns 'distributions.core)
 
-(defn log-det [M]
-  (let [L (:L (la/cholesky M))]
-    (* 2 (reduce + (map log (diagonal L))))))
+
+(defn log-det [M] (reduce + (map log (:S (la/svd M)))))
 
