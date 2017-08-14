09a3e02de1f79b8fc63f311f5e9cad932474a9e0
Michael Lindon <michael.s.lindon@gmail.com>
2017-08-14 14:00:29 -0400

    added truncated inverse gaussian sampling function

--- src/distributions/polya-gamma.clj
+++ src/distributions/polya-gamma.clj
@@ -1,5 +1,30 @@
 (in-ns 'distributions.core)
 
+
+
+(defn rand-tig [mu]
+  (let [t 0.64]
+    (cond
+      (> mu t) (let [z (/ 1 mu)
+                     E (repeatedly #(sample (exponential 1)))
+                     Ep (repeatedly #(sample (exponential 1)))
+                     E-acc (map first (filter (fn [[e ep]] (< (square e) (* 2 (/ ep t) ))) (map vector E Ep)))
+                     X (map (fn [e] (/ t (square (inc (* t e))))) E-acc)
+                     alpha (map (fn [x] (exp (negate (* 0.5 x (square z))))) X)
+                     U (repeatedly #(sample (uniform 0 1)))
+                     accepted (filter (fn [[x a u]] (<= u a)) (map vector X alpha U))]
+                 (first (first accepted)))
+      :else  (let [Y (repeatedly #(square (sample (normal 0 1))))
+                   X (map (fn [y] (+ mu
+                                     (* 0.5 (square mu) y)
+                                     (negate
+                                      (* 0.5
+                                         mu
+                                         (sqrt (+ (* 4 mu y)  (square (* mu y)))))))) Y)
+                   X-new (map (fn [x] (if (> (sample (uniform 0 1)) (/ mu (+ mu x))) (/ (square mu) x) x)) X)
+                   accepted (filter (fn [x] (<= x t)) X-new)]
+               (first accepted)))))
+
 (defn jacobi-a
   ([x n z] (* (cosh z) (exp (negate (* z z 0.5 x))) (jacobi-a x n)))
   ([x n]
@@ -22,8 +47,10 @@
         q (* (cosh z) 0.5 Math/PI (mean ex) (- 1 (cdf ex t)))
         ig-prob (/ p (+ p q))]
     (loop []
-      (let [g (mixture [(truncated ig 0 t) (truncated ex t Double/POSITIVE_INFINITY)] [ig-prob (- 1 ig-prob)])
-            X (sample g)
+      (let [
+            g (mixture [(truncated ig 0 t) (truncated ex t Double/POSITIVE_INFINITY)] [ig-prob (- 1 ig-prob)])
+            ;X (sample g)
+            X (if (< (sample (uniform 0 1)) ig-prob) (rand-tig (inv (abs z))) (sample (truncated ex t Double/POSITIVE_INFINITY)))
             U (sample (uniform 0 (* (+ p q) (pdf g X))))
             stream (drop 1 (iterate (partial jacobi-iterator X z) [(jacobi-a X 0 z) 0]))
             S (first (drop-while (fn [[sn n]] (not (if (odd? n) (< U sn) (> U sn)))) stream))]
