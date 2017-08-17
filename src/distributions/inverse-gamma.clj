7974803c3d7c36bf77cebb8d53914d9a1708330a
Michael Lindon <michael.s.lindon@gmail.com>
2017-08-17 13:29:37 -0400

    correct negative values for inverse gamma

--- src/distributions/inverse-gamma.clj
+++ src/distributions/inverse-gamma.clj
@@ -10,18 +10,18 @@
   (P [d x] (.probability (new GammaDistribution shape (/ 1 scale)) x))
   (P [d] (fn [x] (P d x)))
   distribution-function
-  (cdf [d x] (- 1 (.cumulativeProbability (new GammaDistribution shape (/ 1 scale)) (/ 1 x))))
+  (cdf [d x] (if (<= x 0) 0.0  (- 1 (.cumulativeProbability (new GammaDistribution shape (/ 1 scale)) (/ 1 x)))))
   (cdf [d] (fn [x] (cdf d x)))
   inverse-distribution-function
-  (icdf [d x] (/ 1 (.inverseCumulativeProbability (new GammaDistribution shape (/ 1 scale)) (- 1 x))))
+  (icdf [d x] (if (= x 0) 0.0 (/ 1 (.inverseCumulativeProbability (new GammaDistribution shape (/ 1 scale)) (- 1 x)))))
   (icdf [d] (fn [x] (icdf d x)))
   density-function
-  (pdf [d x] (* (/ 1 (* x x)) (.density (new GammaDistribution shape (/ 1 scale)) (/ 1 x))))
+  (pdf [d x] (if (<= x 0) 0.0 (* (/ 1 (* x x)) (.density (new GammaDistribution shape (/ 1 scale)) (/ 1 x)))))
   (pdf [d] (fn [x] (pdf d x)))
-  (log-pdf [d x] (+ (* -2 (Math/log x)) (.logDensity (new GammaDistribution shape (/ 1 scale)) (/ 1 x))))
+  (log-pdf [d x] (if (<= x 0) Double/NEGATIVE_INFINITY (+ (* -2 (Math/log x)) (.logDensity (new GammaDistribution shape (/ 1 scale)) (/ 1 x)))))
   (log-pdf [d] (fn [x] (log-pdf d x)))
   support
-  (support-lower [d] 0)
+  (support-lower [d] 0.0)
   (support-upper [d] Double/POSITIVE_INFINITY)
   first-moment
   (mean [d] (/ scale (dec shape)))
